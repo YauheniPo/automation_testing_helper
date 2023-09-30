448994a591edf712647322f29d2606c482c8f0f6
YauheniPo <ip.popovich@mail.ru>
2023-09-30 13:20:09 +0200

    added detekt.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,41 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
-    application
+	kotlin("jvm") version "1.9.0"
+	id("io.gitlab.arturbosch.detekt") version Versions.detektGradlePlugin
+	application
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+	testImplementation(kotlin("test"))
+	detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:${Versions.detektGradlePlugin}")
+	detektPlugins("io.gitlab.arturbosch.detekt:detekt-rules-libraries:${Versions.detektGradlePlugin}")
+	detektPlugins("io.gitlab.arturbosch.detekt:detekt-rules-ruleauthors:${Versions.detektGradlePlugin}")
 }
 
 tasks.test {
-    useJUnitPlatform()
+	useJUnitPlatform()
 }
 
 kotlin {
-    jvmToolchain(8)
+	jvmToolchain(8)
 }
 
 application {
-    mainClass.set("MainKt")
+	mainClass.set("MainKt")
+}
+
+allprojects {
+	
+	detekt {
+		source = objects.fileCollection().from(io.gitlab.arturbosch.detekt.extensions.DetektExtension.DEFAULT_SRC_DIR_JAVA, io.gitlab.arturbosch.detekt.extensions.DetektExtension.DEFAULT_TEST_SRC_DIR_JAVA, io.gitlab.arturbosch.detekt.extensions.DetektExtension.DEFAULT_SRC_DIR_KOTLIN, io.gitlab.arturbosch.detekt.extensions.DetektExtension.DEFAULT_TEST_SRC_DIR_KOTLIN)
+		buildUponDefaultConfig = true
+		baseline = file("${rootProject.rootDir}/gradle/detekt/baseline.xml")
+		config = files("${rootProject.rootDir}/gradle/detekt/detekt.yml")
+	}
 }
